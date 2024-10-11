cf4e787d6f59611997e9e3ad3cbc52b41d18ccfe
Vedant Indurkar <indurkarvedant1@gmail.com>
2024-10-11 18:39:36 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 //to establish db connection we need this file
 const mongoose=require('mongoose');
-
+require("dotenv").config();
 //define the mongodb URL
-const mongoURL='mongodb://localhost:27017/hotels'
+//const mongoURL=process.env.MONGODB_URL_LOCAL;
+
+const mongoURL =process.env.MONGODB_URL;
 
 //setup mongoDB connection
 mongoose.connect(mongoURL)
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const db=require('./db');
+require('dotenv').config();
 
 const bodyParser=require('body-parser');
 app.use(bodyParser.json());
 
+const PORT=process.env.PORT || 5000;
 
 const menuItem= require('./models/menuItem');
 
